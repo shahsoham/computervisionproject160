b140039f79177dfe103ffaa427bf8ed15fb24e38
bunditHongmanee <bundit.j.h@gmail.com>
2017-05-09 23:28:05 -0700

    eyelike

---
+++ eyeLike/EyePoints.py
@@ -0,0 +1,94 @@
+import sys
+from subprocess import call
+import os
+import psycopg2
+
+
+"""
+argv[1] = filepath to directory of folder of frames
+argv[2] = videoID
+"""
+def main(argv):
+	if(len(sys.argv) != 3):
+		print("Error! Invalid number of arguments")
+		sys.exit(2) 
+	elif(sys.argv[1][0] != '/' and sys.argv[1][len(sys.argv[1]) - 1] != '/'):
+		print("Error! Not a valid directory")
+		sys.exit(2)
+	else: #make sure there's an entry for the input videoID
+		try:
+			conn=psycopg2.connect("dbname='cs160' user='postgres' host='localhost' password='student'")
+		except:
+			print("Unable to connect to the database.")
+		cur = conn.cursor()
+		cur.execute("""SELECT * FROM "Video" where videoid=%s""", (sys.argv[2], ))
+		if(cur.fetchone() == None):
+			print("No such videoID in the database")
+			sys.exit(2)
+		else:
+			imageDirectory = sys.argv[1]
+			videoID = sys.argv[2]
+	# Retrieve frame count from DB and save in frame_count
+	cur.execute("""SELECT frame_count FROM "Video"
+				where videoid=%s""", (videoID,))
+	values = cur.fetchone()
+	frame_count = values[0]
+
+	outFile = "/home/bundit/Desktop/~project/eyeLike/EyeCoordinatesOutput.txt"
+
+	#Delete file if exists to write new coorindates to new file
+	try:
+		os.remove(outFile)
+	except OSError:
+		pass
+
+	#Process eyepoints and output to file
+	getEyePointsToFile(imageDirectory, frame_count, videoID)
+	#Read back data and insert into database
+	insertEyePointsToDB(outFile, videoID, cur, conn)
+
+	conn.close()
+
+"""
+	Processes the files in a directory and dumps the eye coordinates data into a text file
+	@imageDirectory the directory with all the images to process
+	@frame_count the number of frames in the directory
+	@videoID ID for database
+"""	
+def getEyePointsToFile(imageDirectory, frame_count, videoID):
+
+	for frame in range(1, frame_count + 1):
+		#images follow naming convention videoId.frame_number.jpg
+		imagePath = imageDirectory + str(videoID) + '.' + str(frame) + '.jpg'
+		#print imagePath + "\n"
+		call(['/home/bundit/Desktop/~project/eyeLike/build/bin/./eyeLike', imagePath, '1', str(frame)])
+
+"""
+	Inserts the eye coordinates in the database 
+	@file the file to retrieve data from
+	@videoID key for database inserts
+	@cur cursor used for database access
+"""
+def insertEyePointsToDB(file, videoID, cur, conn):
+	
+	#Check if file exists, if not then exit
+	if not os.path.exists(file):
+		print "EyeCoordinatesOutput.txt could not be found"
+		sys.exit()
+	#Open the file and read line by line
+	try:
+		with open(file) as f:
+			for line in f:
+				currentline = line.split(",")
+				frameNum = currentline[0]
+				rightX = currentline[1]
+				rightY = currentline[2]
+				leftX = currentline[3]
+				leftY = currentline[4]
+				#Insert data into database
+				cur.execute("""INSERT into "PupilData"(videoID, frame_number, leftFabianPupil, rightFabianPupil) VALUES (%s, %s, Point(%s,%s), Point(%s,%s));""",(videoID, frameNum, rightX, rightY, leftX, leftY))
+				conn.commit()
+	except:
+		print "Could not import eye coordinates into database - Error"
+if __name__ == "__main__":
+	main(sys.argv[1:])
---
+++ eyeLike/src/main.cpp
@@ -0,0 +1,193 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <unistd.h>
+
+#include <fstream>
+#include <iostream>
+#include <queue>
+#include <stdio.h>
+#include <math.h>
+#include <sstream>
+#include <string>
+
+#include "constants.h"
+#include "findEyeCenter.h"
+#include "findEyeCorner.h"
+
+using namespace std;
+using namespace cv;
+
+
+
+/** Constants **/
+
+
+/** Function Headers */
+void detectAndDisplay( cv::Mat frame, const std::string& path,const std::string& frame_num );
+
+/** Global variables */
+//-- Note, either copy these two files from opencv/data/haarscascades to your current folder, or change these locations
+//cv::String face_cascade_name = "../../res/haarcascade_frontalface_alt.xml";
+cv::String face_cascade_name = "/home/bundit/Desktop/~project/eyeLike/res/haarcascade_frontalface_alt.xml";
+cv::CascadeClassifier face_cascade;
+std::string main_window_name = "Capture - Face detection";
+std::string face_window_name = "Capture - Face";
+cv::RNG rng(12345);
+cv::Mat debugImage;
+cv::Mat skinCrCbHist = cv::Mat::zeros(cv::Size(256, 256), CV_8UC1);
+
+/**
+ * @function main
+ * arg[1] = file path 
+ * arg[2] = videoID
+ * arg[3] = frame number
+ */
+int main( int argc, const char** argv ) {
+
+  cv::Mat frame;
+
+  // Load the cascades from xml doc
+  if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading face cascade, please change face_cascade_name in source code.\n"); return -1; };
+
+  //open frame for processing
+  frame = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+
+  if( !frame.empty() ) {
+    detectAndDisplay( frame , argv[1], argv[3]);
+  }
+  else {
+    printf("Error uploading photo, frame empty\n");
+  }
+
+
+  return 0;
+}
+
+void findEyes(cv::Mat frame_gray, cv::Rect face, const std::string& path, const std::string& frame_num) {
+  cv::Mat faceROI = frame_gray(face);
+  cv::Mat debugFace = faceROI;
+
+  if (kSmoothFaceImage) {
+    double sigma = kSmoothFaceFactor * face.width;
+    GaussianBlur( faceROI, faceROI, cv::Size( 0, 0 ), sigma);
+  }
+  //-- Find eye regions and draw them
+  int eye_region_width = face.width * (kEyePercentWidth/100.0);
+  int eye_region_height = face.width * (kEyePercentHeight/100.0);
+  int eye_region_top = face.height * (kEyePercentTop/100.0);
+  cv::Rect leftEyeRegion(face.width*(kEyePercentSide/100.0),
+                         eye_region_top,eye_region_width,eye_region_height);
+  cv::Rect rightEyeRegion(face.width - eye_region_width - face.width*(kEyePercentSide/100.0),
+                          eye_region_top,eye_region_width,eye_region_height);
+
+  //-- Find Eye Centers
+  cv::Point leftPupil = findEyeCenter(faceROI,leftEyeRegion,"Left Eye");
+  cv::Point rightPupil = findEyeCenter(faceROI,rightEyeRegion,"Right Eye");
+  // get corner regions
+  cv::Rect leftRightCornerRegion(leftEyeRegion);
+  leftRightCornerRegion.width -= leftPupil.x;
+  leftRightCornerRegion.x += leftPupil.x;
+  leftRightCornerRegion.height /= 2;
+  leftRightCornerRegion.y += leftRightCornerRegion.height / 2;
+  cv::Rect leftLeftCornerRegion(leftEyeRegion);
+  leftLeftCornerRegion.width = leftPupil.x;
+  leftLeftCornerRegion.height /= 2;
+  leftLeftCornerRegion.y += leftLeftCornerRegion.height / 2;
+  cv::Rect rightLeftCornerRegion(rightEyeRegion);
+  rightLeftCornerRegion.width = rightPupil.x;
+  rightLeftCornerRegion.height /= 2;
+  rightLeftCornerRegion.y += rightLeftCornerRegion.height / 2;
+  cv::Rect rightRightCornerRegion(rightEyeRegion);
+  rightRightCornerRegion.width -= rightPupil.x;
+  rightRightCornerRegion.x += rightPupil.x;
+  rightRightCornerRegion.height /= 2;
+  rightRightCornerRegion.y += rightRightCornerRegion.height / 2;
+  //rectangle(debugFace,leftRightCornerRegion,200);
+  //rectangle(debugFace,leftLeftCornerRegion,200);
+  //rectangle(debugFace,rightLeftCornerRegion,200);
+  //rectangle(debugFace,rightRightCornerRegion,200);
+  // change eye centers to face coordinates
+  rightPupil.x += rightEyeRegion.x;
+  rightPupil.y += rightEyeRegion.y;
+  leftPupil.x += leftEyeRegion.x;
+  leftPupil.y += leftEyeRegion.y;
+  // draw eye centers
+  circle(debugFace, rightPupil, 3, 1234);
+  circle(debugFace, leftPupil, 3, 1234);
+  
+  //output image with regions drawn
+  //imwrite("frame.png",frame_gray);
+
+  //write the coordinates of left and right pupils to console
+  //printf("Right Pupil Coordinates: X - %d Y - %d \n", rightPupil.x, rightPupil.y);
+  //printf("Left  Pupil Coordinates: X - %d Y - %d \n", leftPupil.x, leftPupil.y);
+
+  std::ofstream outfile;
+  outfile.open("/home/bundit/Desktop/~project/eyeLike/EyeCoordinatesOutput.txt", std::ofstream::out | std::ofstream::app);
+  //outfile.open(&path, std::ofstream::out | std::ofstream::app);
+  outfile << frame_num;
+  outfile << ",";
+  outfile << rightPupil.x;
+  outfile << ",";
+  outfile << rightPupil.y;
+  outfile << ",";
+  outfile << leftPupil.x;
+  outfile << ",";
+  outfile << leftPupil.y;
+  outfile << "\n";
+  
+  outfile.close();
+
+}
+/**
+ * @function detectAndDisplay
+ */
+void detectAndDisplay( cv::Mat frame , const std::string& path,const std::string& frame_num) {
+  std::vector<cv::Rect> faces;
+  //cv::Mat frame_gray;
+
+  std::vector<cv::Mat> rgbChannels(3);
+  cv::split(frame, rgbChannels);
+  cv::Mat frame_gray = rgbChannels[2];
+
+  //cvtColor( frame, frame_gray, CV_BGR2GRAY );
+  //equalizeHist( frame_gray, frame_gray );
+  //cv::pow(frame_gray, CV_64F, frame_gray);
+  //-- Detect faces
+  face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE|CV_HAAR_FIND_BIGGEST_OBJECT, cv::Size(150, 150) );
+//  findSkin(debugImage);
+
+  for( int i = 0; i < faces.size(); i++ )
+  {
+    //rectangle(debugImage, faces[i], 1234);
+  }
+  //-- Show what you got
+  if (faces.size() > 0) {
+    findEyes(frame_gray, faces[0], path, frame_num);
+  }
+}
+
+cv::Mat findSkin (cv::Mat &frame) {
+  cv::Mat input;
+  cv::Mat output = cv::Mat(frame.rows,frame.cols, CV_8U);
+
+  cvtColor(frame, input, CV_BGR2YCrCb);
+
+  for (int y = 0; y < input.rows; ++y) {
+    const cv::Vec3b *Mr = input.ptr<cv::Vec3b>(y);
+//    uchar *Or = output.ptr<uchar>(y);
+    cv::Vec3b *Or = frame.ptr<cv::Vec3b>(y);
+    for (int x = 0; x < input.cols; ++x) {
+      cv::Vec3b ycrcb = Mr[x];
+//      Or[x] = (skinCrCbHist.at<uchar>(ycrcb[1], ycrcb[2]) > 0) ? 255 : 0;
+      if(skinCrCbHist.at<uchar>(ycrcb[1], ycrcb[2]) == 0) {
+        Or[x] = cv::Vec3b(0,0,0);
+      }
+    }
+  }
+
+  return output;
+}
